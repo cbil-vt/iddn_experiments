3ae134d63de51869fec24025cde5bcd8f5b6697b
Yizhi Wang <yzwang@vt.edu>
2025-02-05 17:21:23 -0500

    reorganize

---
+++ rev0/s0_test_large_data.py
@@ -0,0 +1,63 @@
+# %%
+# test iDDN on a large data
+
+from importlib import reload
+import numpy as np
+import matplotlib.pyplot as plt
+import muon as mu
+from iddn import iddn, solver
+from iddn_data import load_data
+
+reload(iddn)
+reload(solver)
+
+
+# %%
+
+mdata = mu.read("../../iddn_play/muon/data/pbmc10k.h5mu")
+
+# %%
+
+
+
+# %%
+
+rna = mdata.mod['rna']
+data:np.ndarray = rna.X
+
+dat1 = data[:1000].astype(np.float64)
+dat2 = data[1000:2000].astype(np.float64)
+
+s1 = np.std(dat1,axis=0)
+s2 = np.std(dat2,axis=0)
+
+idx_good = (s1>0.1) * (s2>0.1)
+dat1 = dat1[:,idx_good]
+dat2 = dat2[:,idx_good]
+
+# %%
+
+n_gene = dat1.shape[1]
+print(n_gene)
+dep_mat = np.zeros((n_gene, n_gene), dtype=np.int8)
+dep_mat[:2000,:] = 1
+l1_mat = 0.15
+l2_mat = 0.02
+# l1_mat = np.zeros((n_gene, n_gene))+0.15
+# l2_mat = np.zeros((n_gene, n_gene))+0.02
+
+# %%
+
+out_iddn = iddn.iddn_parallel(
+    dat1,
+    dat2,
+    dep_mat=dep_mat,
+    lambda1=l1_mat,
+    lambda2=l2_mat,
+    n_process=8,
+    output_sparse=True,
+)
+
+
+# %%
+
